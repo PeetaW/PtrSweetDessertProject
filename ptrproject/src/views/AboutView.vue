8aeebf638e9addff841c4a089f07e4b8b65be8e0
PeetaW <h7337473@hotmail.com.tw>
2025-02-11 00:48:40 +0800

    continue developing "about me" page

--- ptrproject/src/views/AboutView.vue
+++ ptrproject/src/views/AboutView.vue
@@ -1,29 +1,100 @@
 <template>
-  <div class="lobbyContainer">
-    <div class="lobbyImage">
-      <img src="../assets/image/image-lobby-forest.jpg" alt="">
-      <div class="leftDirection">
-        <div class="leftTitle">小舖介紹</div>
-        <div class="leftarrow"><font-awesome-icon :icon="['fas', 'arrow-left']" /></div>
-      </div>
-      <div class="rightDirection">
-        <div class="rightTitle">甜點特色</div>
-        <div class="rightarrow"><font-awesome-icon :icon="['fas', 'arrow-right']" /></div>
+  <div class="viewBox">
+    <div class="infoContainers" v-bind:style="containerStyle">
+      <div class="lobbyContainer room" v-for="image,id in images" v-bind:key="id">
+        <div class="lobbyImage">
+          <img v-bind:src="require(`@/assets/image/${image.image}`)" alt="">
+          <div class="leftDirection" v-on:click="turnLeft">
+            <div class="leftTitle">小舖介紹</div>
+            <hr>
+            <div class="leftarrow"><font-awesome-icon :icon="['fas', 'arrow-left']" /></div>
+          </div>
+          <div class="rightDirection" v-on:click="turnRight">
+            <div class="rightTitle">甜點特色</div>
+            <hr>
+            <div class="rightarrow"><font-awesome-icon :icon="['fas', 'arrow-right']" /></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+// import $ from 'jquery'
+
+export default {
+  data () {
+    return {
+      page: 1,
+      images: [
+        {
+          name: 'Store intro',
+          image: 'image-store-intro.jpg'
+        },
+        {
+          name: 'Lobby forest',
+          image: 'image-lobby-forest.jpg'
+        },
+        {
+          name: 'Cuisine intro',
+          image: 'image-cuisine-intro.jpg'
+        }
+      ]
+    }
+  },
+  computed: {
+    containerStyle () {
+      return {
+        transform: `translateX(${-this.page * 100}%)`,
+        transition: 'ease-in-out 0.5s'
+      }
+    }
+  },
+  methods: {
+    turnRight () {
+      if (this.page < this.images.length - 1) {
+        this.page += 1
+      }
+    },
+    turnLeft () {
+      if (this.page > 0) {
+        this.page -= 1
+      }
+    }
+
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 * {
+  // border: solid 1px black;
+}
+
+div.viewBox {
+  width: 100%;
+  height: 90vh;
   border: solid 1px black;
+  overflow: hidden;
+  // &:hover div.infoContainers {
+  //   transform: translateX(-100%);
+  // }
+}
+
+div.infoContainers {
+  white-space: nowrap;
+  transition: linear 1s;
 }
 
 div.lobbyContainer {
   width: 100%;
   height: 90vh;
+  display: inline-block;
+}
+
+div.lobbyImage {
+  // border: solid 1px red;
   position: relative;
 }
 
@@ -35,13 +106,36 @@ img {
   position: relative;
 }
 
+hr {
+  margin: 0;
+  border-color: #AAB9CC;
+  transition:cubic-bezier(0.39, 0.575, 0.565, 1) 0.5s;
+}
+
 div.leftDirection, div.rightDirection {
   display: inline-block;
   position: absolute;
+  border-radius: 5px;
+  transform: translateX(-50%) translateY(50%);
+  background-color: #698C88;
+  transition:cubic-bezier(0.455, 0.03, 0.515, 0.955) 0.3s;
+  cursor: pointer;
+  font-size: 1.3rem;
+  &:hover {
+    background-color: #AAB9CC;
+    hr {
+      border-color: #698C88;
+    }
+  }
 }
 
 div.leftDirection {
-  left: 0;
+  left: 33%;
+  bottom: 0;
+}
+
+div.rightDirection {
+  right: 33%;
   bottom: 0;
 }
 </style>
